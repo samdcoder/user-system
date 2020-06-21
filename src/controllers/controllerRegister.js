21cbc34f780336baf35dc1b5d12e70f38b51cc27
= <sameer.pathan@peopleinteractive.in>
2020-06-21 17:34:32 +0530

    integrated axios

--- src/controllers/controllerRegister.js
+++ src/controllers/controllerRegister.js
@@ -1,4 +1,23 @@
-export default (data) => {
-  console.log('data in controller => ', data)
-  //intergrate axios
+const axios = require('axios').default;
+
+export default (d) => {
+  console.log('data in controller => ', d)
+  const {userName: username, password, email, phone: contact} = d;
+  const url = 'http://localhost/php-mvc/user/register';
+  const headers = {'content-type': 'application/x-www-form-urlencoded'};
+  const data = {
+    headers,
+    username,
+    email,
+    password,
+    contact
+  };
+  
+  axios.post(url, data, {headers: headers})
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 }
