5a4198b5342111b6e82727911fe78e6a90fbb495
= <sameer.pathan@peopleinteractive.in>
2020-06-21 12:55:32 +0530

    added sign up component

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import SignUp from "../components/SignUp";
 
 
 export default class Register extends React.Component {
+  t
+  
+  /*
+    user name
+    password
+    email
+    contact
+   */
   render() {
     return (
       <div>
-        Register Page
+        <SignUp/>
       </div>
     );
   }
